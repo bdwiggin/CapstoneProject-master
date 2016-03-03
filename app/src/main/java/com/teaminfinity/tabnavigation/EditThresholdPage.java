38f761d9be5822be8659185372c48fe5880eed0c
BrandonWiggins <Bw256402242>
2016-03-02 21:32:46 -0500

    Initial commit

---
+++ app/src/main/java/com/teaminfinity/tabnavigation/EditThresholdPage.java
@@ -0,0 +1,15 @@
+package com.teaminfinity.tabnavigation;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by BrandonWiggins on 25/02/2016.
+ */
+public class EditThresholdPage extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_threshold_page);
+    }
+}
